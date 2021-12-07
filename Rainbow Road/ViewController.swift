cf2e8da47714e36a9b3000cb1af2a13dcdecc4b3
Latifa881 <l-n-a-m@hotmail.com>
2021-12-07 11:40:56 -0800

    first commit

--- Rainbow Road/ViewController.swift
+++ Rainbow Road/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var colors:[UIColor]=[.red,
+                          .green,
+                          .yellow,
+                          .systemTeal,
+                          .blue,
+                          .purple]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    
+    
 }
 
+extension ViewController: UITableViewDataSource , UITableViewDelegate{
+    //Datasource
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return an integer that indicates how many rows (cells) to draw
+        return colors.count
+    }
+    
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            
+            return 120
+        }
+    
+}
